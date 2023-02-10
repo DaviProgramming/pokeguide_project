5fe32408cf7ce6883776260eab3b866eff791d28
David Oliveira <queriaprogramar@gmail.com>
2023-02-10 12:48:00 -0300

    primeiro commit e updates na start page

---
+++ src/components/startComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="container start">
+    <h1>PokéGuide</h1>
+   
+    
+
+    <img src="../assets/pokemons.png" alt="">
+    <form>
+
+        <div class="form-control">
+          <input type="text" class="form-input" placeholder="Type a Pókemon Name" />
+          <div class="input-options">
+            <div class="option">Pikachu </div>
+            <div class="option">Pikachu </div>
+            <div class="option">Pikachu </div>
+            <div class="option">Pikachu </div>
+          </div>
+        </div>
+  
+        <button type="button"><i class="fa-solid fa-magnifying-glass"></i></button>
+  
+      </form>
+  </section>
+
+
+</template>
+
+<script>
+export default {
+  name: "startComponent",
+};
+</script>
